dd832cc092d4122b00125022e16e30b230c01c87
E. Lynette Rayle <elr37@cornell.edu>
2015-02-21 21:21:20 -0500

    FIX: error when ASCII-8BIT coding characters are in marcxml

--- lib/ld4l/works_rdf/services/conversion_services/get_rdfxml_from_marcxml.rb
+++ lib/ld4l/works_rdf/services/conversion_services/get_rdfxml_from_marcxml.rb
@@ -12,6 +12,8 @@ module LD4L
         raise ArgumentError, 'marcxml argument must be a non-empty string'  unless
             marcxml.kind_of?(String) && marcxml.size > 0
 
+        marcxml = marcxml.force_encoding('ASCII-8BIT').encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?')
+
         # TODO: Where should tmp files be saved?  The following puts them in the lib directory
         uuid = SecureRandom.uuid
         rdfxml_file  = File.join(File.dirname(__FILE__), "tmp_#{uuid}.rdf")
